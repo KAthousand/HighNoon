4505e48547da6d04ae9c85f0008f4674e9838a3d
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-12-01 15:28:15 -0500

    services folder setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import { Route } from "react-router-dom";
+import Login from "./screens/Login/Login";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Route path="/">
+        <Login />
+      </Route>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/screens/services/scores.js
@@ -0,0 +1,25 @@
+import api from "./apiConfig";
+
+export const getAllScores = async () => {
+  const resp = await api.get("/scores");
+  return resp.data;
+};
+
+// export const getOneScore = async (id) => {
+//   const resp = await api.get(`/scores/${id}`);
+//   return resp.data;
+// };
+
+export const postScore = async (scoreData) => {
+  const resp = await api.post("/scores", { score: scoreData });
+  return resp.data;
+};
+
+// export const putScore = async (id, scoreData) => {
+//   const resp = await api.put(`/scores/${id}`, {score: scoreData})
+// }
+
+export const deleteScore = async (id) => {
+  const resp = await api.delete(`/scores/${id}`);
+  return resp;
+};
