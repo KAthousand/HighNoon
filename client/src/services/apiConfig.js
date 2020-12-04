d396bcbc0f5233a703546376134e17be9e39bb76
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-12-04 18:28:36 -0500

    adding production URL

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3000";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://high-noon.herokuapp.com/"
+    : "http://localhost:3000";
 
 // put a ternary for production
 
